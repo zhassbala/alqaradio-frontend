545c8dc0479406613f39b2f5048ea20de4aa4b11
Rassul Bessimbekov <besimbekovr@gmail.com>
2024-05-30 20:06:32 +0500

    feat: add locale parameter to api calls

--- src/api/about-us.ts
+++ src/api/about-us.ts
@@ -1,13 +1,21 @@
 import { IAboutUsPage } from "@/types";
 import { api } from "./api";
+import { defaultLocale } from "@/locales";
 
-export const getAboutUsPage = async (): Promise<{ data: IAboutUsPage }> => {
-  const { data } = await api.get("/about-us");
+export const getAboutUsPage = async (locale = defaultLocale): Promise<{ data: IAboutUsPage }> => {
+  const { data } = await api.get("/about-us", {
+    params: {
+      locale,
+    },
+  });
   return data;
 };
-export const getAboutUsMediaPage = async (): Promise<{
-  data: IAboutUsPage;
-}> => {
-  const { data } = await api.get("/about-us-media");
+
+export const getAboutUsMediaPage = async (locale = defaultLocale): Promise<{ data: IAboutUsPage }> => {
+  const { data } = await api.get("/about-us-media", {
+    params: {
+      locale,
+    },
+  });
   return data;
 };
--- src/api/articles.ts
+++ src/api/articles.ts
@@ -1,22 +1,25 @@
 import type { IResponse, IArticle } from "@/types";
 import { api } from "./api";
+import { defaultLocale } from "@/locales";
 
 export const getArticles = async (
+  locale = defaultLocale,
   options: {
     limit?: number;
   } = {}
 ): Promise<IResponse<IArticle[]>> => {
   const { data } = await api.get("/articles", {
     params: {
+      locale,
       populate: "*",
       sort: "Date",
+      "pagination[start]": 0,
+      "pagination[limit]": options.limit,
     },
   });
   return data;
 };
-export const getArticle = async (
-  articleSlug: string
-): Promise<IResponse<IArticle[]>> => {
+export const getArticle = async (articleSlug: string): Promise<IResponse<IArticle[]>> => {
   const { data } = await api.get("/articles/", {
     params: {
       populate: "*",
