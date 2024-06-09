ff0611a528fbf5c038c6029eae60637bb896e625
Rassul Bessimbekov <besimbekovr@gmail.com>
2024-06-10 02:18:53 +0500

    feat: add blurred bg for image + wrap content with white bg in news page

--- src/app/[lang]/articles/[article-slug]/page.tsx
+++ src/app/[lang]/articles/[article-slug]/page.tsx
@@ -19,23 +19,30 @@ export default async function Article(page: Page<Params>) {
     return <NotFound></NotFound>;
   }
   return (
-    <div className="max-w-[1280px]">
-      <h1 className="text-3xl font-semibold mb-8">
+    <div className="max-w-[1280px] p-4">
+      <h1 className="text-2xl md:text-3xl text-center md:text-left font-semibold mb-8">
         {article.attributes.Title}
       </h1>
       {article.attributes.Cover?.data && (
-        <Image
-          className="object-cover w-full max-w-[800px] mb-4"
-          width={article.attributes.Cover.data.attributes.width}
-          height={article.attributes.Cover.data.attributes.height}
-          src={getImagePath(article.attributes.Cover.data.attributes.url)}
-          alt={article.attributes.Cover.data.attributes.name}
-        ></Image>
+        <div className="w-full backdrop-blur-lg rounded-xl relative">
+          <div
+            className="w-full h-full absolute top-0 left-0 blur opacity-60"
+            style={{
+              backgroundImage: `url(${getImagePath(article.attributes.Cover.data.attributes.url)})`,
+            }}
+          />
+
+          <Image
+            className="object-cover w-full max-w-[800px] mb-4 mx-auto relative z-10"
+            width={article.attributes.Cover.data.attributes.width}
+            height={article.attributes.Cover.data.attributes.height}
+            src={getImagePath(article.attributes.Cover.data.attributes.url)}
+            alt={article.attributes.Cover.data.attributes.name}
+          ></Image>
+        </div>
       )}
-      <div>
-        <BlocksRenderer
-          content={article.attributes.Content ?? []}
-        ></BlocksRenderer>
+      <div className="bg-white rounded-xl p-4 md:p-8 shadow-lg">
+        <BlocksRenderer content={article.attributes.Content ?? []}></BlocksRenderer>
       </div>
     </div>
   );
